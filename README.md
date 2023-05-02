e025e7c306482a7ba3034e3c40e2fe27da354cee
Mohammad-Keath <m.ghyath.z@gmail.com>
2023-05-02 10:04:41 +0000

    solving lab 14

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 ## WRRC
 ![WRRC](./photoes/WRRC.jpeg)
 ## Overview
-this website is a movie website that will check the latest movies based on categories
+this website is a movie website that will check the latest movies based on categories and you can search according title
 
 ## Getting Started
 you have to clone it on your machine, then you have to type 'node server.js', after that you can open it on browzer or postman by typing the server number
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+require ("dotenv").config();
 const express = require("express");
 const recipesData = require('./movieData/data.json');
 const app = express();
 const port = 3001;
 const cors = require('cors');
+const axios = require("axios");
 app.use(cors());
+const api_key = process.env.API_KEY;
 let result = {'title' : recipesData.title,
 'poster_path' : recipesData.poster_path , 
 'overview' : recipesData.overview};
+
+function listItem(id,title,release_date,poster_path,overview){
+    this.id = id;
+    this.title = title;
+    this.release_date = release_date;
+    this.poster_path=poster_path;
+    this.overview=overview
+}
 
 app.get("/", (req, res) => {
     res.json(result);
@@ -15,13 +26,65 @@ app.get("/", (req, res) => {
 app.get('/favorite', (req, res) => {
     res.send('Welcome to Favorite Page');
 });
+app.get("/trending", handleTrending) 
+async function handleTrending(req,res)  {
+    const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${api_key}&language=en-US`
+        let listFromApi=await axios.get(url);
+        console.log(listFromApi)
+        let list = listFromApi.data.results.map((item) => {
+            return new listItem (item.id,item.title,item.release_date,item.poster_path,item.overview)
+        })
+        res.send(list);
+    };
+
+app.get("/search", handleSearch) 
+async function handleSearch(req,res)  {
+    const search = req.query.title
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${search}`
+    let searchByTitle=await axios.get(url);
+    let list = searchByTitle.data.results.map((item) => {
+        return new listItem (item.id,item.title,item.release_date,item.poster_path,item.overview)
+    })
+        res.send(list);
+    };
+
+app.get("/forchildren", handleForChildren) 
+async function handleForChildren(req,res)  {
+    const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${api_key}&language=en-US`
+    const forChildren = await axios.get(url);
+    let list = forChildren.data.results.map((item) => { if (item.adult == false){
+        return new listItem (item.id,item.title,item.release_date,item.poster_path,item.overview)}
+    })
+        res.send(list);
+    };
+app.get("/top_rated", handleTopRated) 
+    async function handleTopRated(req,res)  {
+        const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=799682632f70878276e93308454e2b1b&language=en-US&page=1`
+        let topRated=await axios.get(url);
+        let list = topRated.data.results.map((item) => { if (item.adult == false){
+            return new listItem (item.id,item.title,item.release_date,item.poster_path,item.overview)}
+        })
+            res.send(list);
+        };
+
+// app.get("/details", handleDetails) 
+//     async function handleDetails(req,res)  {
+//         const list_id = req.query.id
+//         const url = `https://api.themoviedb.org/3/list/${list_id}?api_key=${api_key}&language=en-US`
+//         let details=await axios.get(url);
+//          let list = searchByAge.data.results.map((item) => {
+//              return new listItem (item.id,item.title,item.release_date,item.poster_path,item.overview)
+//          })
+//             res.send(details);
+//         };
+
+    
 app.get("*", (req, res) => {
         res.status(500).send('Sorry, something went wrong');
     });
 app.post('*', (req, res) => {
         res.status(404).send('page not found error');
     });
-
 
 
 app.listen(port, () => {
